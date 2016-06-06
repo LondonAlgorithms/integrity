06fcd9633444b051a53cbd5a41cba4088433235c
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2016-06-06 22:33:25 +0100

    Fixed some pathfinding setup

--- public/pathfinding-javascript/spec.js
+++ public/pathfinding-javascript/spec.js
@@ -7,18 +7,4 @@ describe('problem1', function(){
       expect(problem1(123)).to.eql(8)
     });
   });
-
-  context('Test Case 3', function(){
-    it('it returns the correct coins efficiently', function(){
-      //var x = []
-      //var start;
-      //var finish;
-      //for(var i=0;i<20000; ++i) { x.push(1) }
-      //start = Date.now()
-      //var result = coinChange(20000, [1])
-      //finish = Date.now()
-      //expect(result).to.eql(x)
-      //expect(finish-start).to.be.lessThan(50)
-    });
-  });
 });
