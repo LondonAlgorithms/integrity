5fc978138ac1cf15a6700829f5227745f7277f47
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2016-06-27 22:18:09 +0100

    made the logic a lot simpler

--- app.rb
+++ app.rb
@@ -4,33 +4,15 @@ require 'json'
 
 set :bind, '0.0.0.0'
 
-get "/greedy-javascript" do
+get "/:dir" do
   content_type :json
-  files = Dir.
-            entries("public/greedy-javascript/").
-            reject { |x| [".","..", ".DS_Store"].include?(x) }.
-            collect { |q| "greedy-javascript/" + q }
-
+  files = filter_files(params[:dir])
   {files: files}.to_json
 end
 
-
-get "/pathfinding-javascript" do
-  content_type :json
-  files = Dir.
-            entries("public/pathfinding-javascript/").
-            reject { |x| [".","..", ".DS_Store"].include?(x) }.
-            collect { |q| "pathfinding-javascript/" + q }
-
-  {files: files}.to_json
-end
-
-get "/pathfinding-ruby" do
-  content_type :json
-  files = Dir.
-            entries("public/pathfinding-ruby/").
-            reject { |x| [".","..", ".DS_Store"].include?(x) }.
-            collect { |q| "pathfinding-ruby/" + q }
-
-  {files: files}.to_json
+def filter_files(dir)
+  Dir.
+    entries("public/#{dir}").
+    reject { |x| [".","..", ".DS_Store"].include?(x) }.
+    collect { |q| "#{dir}/" + q }
 end
